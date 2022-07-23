c6ba6b7a99889d96dbb06632dc5c81df646d2db9
umeedchandel <umeedchandel@gmail.com>
2022-07-23 18:56:44 +0530

    Added Categories

--- UmeedPieShop/Controllers/PieController.cs
+++ UmeedPieShop/Controllers/PieController.cs
@@ -4,15 +4,14 @@ using UmeedPieShop.ViewModel;
 
 namespace UmeedPieShop.Controllers
 {
-    public class PieController: Controller
+    public class PieController : Controller
     {
         private readonly IPieRepository _pieRepository;
-        //private readonly ICategoryRepository _categoryRepository;
-
+        private readonly ICategoryRepository _categoryRepository;
         public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
         {
             _pieRepository = pieRepository;
-            //_categoryRepository = categoryRepository;
+            _categoryRepository = categoryRepository;
         }
 
         public IActionResult List()
@@ -33,6 +32,35 @@ namespace UmeedPieShop.Controllers
             var pie = _pieRepository.GetPieById(id);
             return View(pie);
         }
+
+        public IActionResult Category1()
+        {
+            CustomeClass customeClass = new CustomeClass();
+            customeClass.Pies = _pieRepository.AllPies.Where(c => c.CategoryId == 1);
+            customeClass.CurrentCategory = _categoryRepository.AllCategories.Where(c => c.CategoryId == 1).Select(c => c.CategoryName).First();
+            customeClass.CategoryDescription = _categoryRepository.AllCategories.Where(c => c.CategoryId == 1).Select(c=> c.Description).First();
+            return View(customeClass);
+        }
+
+        public IActionResult Category2()
+        {
+            CustomeClass customeClass = new CustomeClass();
+            customeClass.Pies = _pieRepository.AllPies.Where(c => c.CategoryId == 2);
+            customeClass.CurrentCategory = _categoryRepository.AllCategories.Where(c => c.CategoryId == 2).Select(c => c.CategoryName).First();
+            customeClass.CategoryDescription = _categoryRepository.AllCategories.Where(c => c.CategoryId == 2).Select(c => c.Description).First();
+            return View(customeClass);
+        }
+
+        public IActionResult Category3()
+        {
+            CustomeClass customeClass = new CustomeClass();
+            customeClass.Pies = _pieRepository.AllPies.Where(c => c.CategoryId == 3);
+            customeClass.CurrentCategory = _categoryRepository.AllCategories.Where(c => c.CategoryId == 3).Select(c => c.CategoryName).First();
+            customeClass.CategoryDescription = _categoryRepository.AllCategories.Where(c => c.CategoryId == 3).Select(c => c.Description).First();
+            return View(customeClass);
+        }
+
+
 
         //action method for home page - listing all the pies of the week
         //action method for details page 
---
+++ UmeedPieShop/ViewModel/CustomeClass.cs
@@ -0,0 +1,13 @@
+﻿using UmeedPieShop.Models;
+
+namespace UmeedPieShop.ViewModel
+{
+    public class CustomeClass
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; }
+        public string CategoryDescription { get; set; }
+
+
+    }
+}
