c872c4ce177c7399a7fe3b6854c15ccebcca64d8
umeedchandel <umeedchandel@gmail.com>
2022-07-31 02:32:46 +0530

    Tried component api conn

--- UmeedPieShop/Components/CategoryMenu.cs
+++ UmeedPieShop/Components/CategoryMenu.cs
@@ -4,15 +4,20 @@ using UmeedPieShop.Models;
 namespace UmeedPieShop.Components
 {
     // component -> repository -> appdbcontext -> EFcore -> database
-    public class CategoryMenu: ViewComponent // <<=== Equivalent to Controller
+    public class CategoryMenu : ViewComponent // <<=== Equivalent to Controller
     {
         private readonly ICategoryRepository categoryRepository;
-        public CategoryMenu(ICategoryRepository categoryRepository)
+        private readonly IConfiguration _configuration;
+        string baseAddress;
+        public CategoryMenu(ICategoryRepository categoryRepository, IConfiguration configuration)
         {
             this.categoryRepository = categoryRepository;
+            _configuration = configuration;
+            baseAddress = configuration.GetValue<string>("BaseAddress");
         }
         public IViewComponentResult Invoke() // <<=== Equivalent to Action Method
         {
+            //var category = StaticApiData1.GetApiData(baseAddress + "Pie/AllCategories");
             return View(categoryRepository.AllCategories);
         }
     }
